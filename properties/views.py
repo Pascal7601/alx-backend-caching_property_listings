38f87e4bf42d3b5464ad917bf307879775660746
Pascal7601 <levispascal63@gmail.com>
2025-11-01 17:46:02 +0300

    implement caching per view with the django view cache

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Property
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
 
-# Create your views here.
+
+@cache_page(60)
+def property_list(request):
+    properties_qs = Property.objects.values(
+        'title', 
+        'description', 
+        'price', 
+        'created_at'
+    )
+    
+    # Convert the QuerySet to a plain list
+    properties_list = list(properties_qs)
+    return JsonResponse(properties_list, safe=False)
